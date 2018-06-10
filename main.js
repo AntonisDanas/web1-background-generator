f7b31e382f434d7022c4d50a2b41f32eefe14e54
DESKTOP-VNO2FDQ\Anton <antondan.tuc@gmail.com>
2018-06-10 13:15:05 +0300

    Working prototype

--- main.js
+++ main.js
@@ -1,93 +1,19 @@
-var button = document.getElementById("submitButton");
-var input = document.getElementById("inputText");
-var list = document.getElementById("itemList");
+
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var body = document.getElementById("gradient");
+var colorPrint = document.getElementById("colorPrint");
 
 
-var CheckIfInputIsEmpty = function()
-{
-    return input.value.length > 0;
-}
+var ChangeBackground = function () {
+    body.style.background = "linear-gradient(to right, "
+                            + color1.value 
+                            + ", "
+                            + color2.value 
+                            + ")";
+    colorPrint.textContent = color1.value + " " + color2.value;
+};
 
-var SetInputToEmpty = function()
-{
-    input.value = "";
-}
+color1.addEventListener("input", ChangeBackground);
 
-var InsertElementToList = function(value)
-{
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(value));
-    list.appendChild(li);
-}
-
-var InputBySubmit = function()
-{
-    if (CheckIfInputIsEmpty())
-    {
-        InsertElementToList(input.value);
-        SetInputToEmpty()
-    }
-}
-
-var InputByEnter = function(event)
-{
-    if (CheckIfInputIsEmpty() && event.keyCode === 13)
-    {
-        InsertElementToList(input.value);
-        SetInputToEmpty();
-    }
-}
-
-button.addEventListener("click",InputBySubmit);
-input.addEventListener("keypress",InputByEnter);
-
-
-
-
-const array = [1, 2, 3, 4];
-console.log(array);
-const double = [];
-const newArray = array.forEach((num) =>
-{
-    double.push(num*2);
-})
-
-console.log(double);
-
-const mapArray = array.map((num) =>
-{
-    return num*3;
-})
-
-console.log(mapArray);
-
-const filterArray = array.filter(num => num > 2);
-
-console.log(filterArray);
-
-class Player 
-{
-    constructor(name, type)
-    {
-        this.name = name;
-        this.type = type;
-    }
-
-    introduce()
-    {
-        console.log(`Hi! My name is ${this.name}, I\'m a ${this.type}.`);
-    }
-}
-
-class Wizard extends Player
-{
-    constructor(name)
-    {
-        super(name,"Wizard");
-    }
-
-    play()
-    {
-        console.log("I'm playing!!");
-    }
-}
+color2.addEventListener("input", ChangeBackground);
